30dcd0d5fc7b3551d6ed63da57ee0b35858c38cc
52liyitong <zhangxun@westlake.edu.cn>
2025-05-24 12:09:48 +0800

    CNN\FCN\data_clean\data_aug

---
+++ project_road_segmentation/public_kaggle_files/Classification.py
@@ -0,0 +1,5 @@
+# in this file we will classify the images's  pixel into road and non-road using three different models
+#1 logistic regression
+#2 CNN
+#3 Transfomer
+
---
+++ project_road_segmentation/public_kaggle_files/Data_processing.py
@@ -0,0 +1,1 @@
+#in this file we will process the data for the classification
